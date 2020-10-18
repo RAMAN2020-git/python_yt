242b842e38a2554591a04a5290164576f7f93d5a
Raman <profazpcb@gmail.com>
2020-10-18 12:59:20 -0400

    fixed da downloader
    deleted 2 old files and updated yt.py :)

--- yt.py
+++ yt.py
@@ -13,7 +13,7 @@ caption = yt.captions.get_by_language_code('en')
 #Getting the highest resolution possible
 ys = yt.streams.get_highest_resolution()
 print(caption.generate_srt_captions())
-f = open(str("demofile2.txt"), "w")
+f = open(str("captions.txt"), "w")
 f.write(caption.generate_srt_captions())
 f.close()
 
